44c9f30af7675103e66ab3d0e0f63d74c96a247a
Rafhael Payão <51467263+rafaeltmbr@users.noreply.github.com>
2020-11-24 11:14:55 -0300

    feat: add background color

--- scripts.js
+++ scripts.js
@@ -1,11 +1,11 @@
-window.addEventListener('load', () => {
+window.addEventListener("load", () => {
   implementControlCubeMovement();
   implementRubiksCubePiecesMovement();
 });
 
 function implementControlCubeMovement() {
-  const controlCube = document.querySelector('.control-cube');
-  const rubiksCube = document.querySelector('.rubiks-cube');
+  const controlCube = document.querySelector(".control-cube");
+  const rubiksCube = document.querySelector(".rubiks-cube");
   const maxXAngle = 20;
   const lastOffset = { x: -45, y: maxXAngle };
   const offset = {};
@@ -18,11 +18,18 @@ function implementControlCubeMovement() {
     offset.x = lastOffset.x;
 
     function handleCubeMovement(moveEvent) {
-      const { pageX: pX, pageY: pY } = isTouch ? moveEvent.touches[0] : moveEvent;
+      const { pageX: pX, pageY: pY } = isTouch
+        ? moveEvent.touches[0]
+        : moveEvent;
       offset.x += pX - last.x;
       offset.y += pY - last.y;
       offset.x = offset.x % 360;
-      offset.y = offset.y >= maxXAngle ? maxXAngle : offset.y <= -maxXAngle ? -maxXAngle : offset.y;
+      offset.y =
+        offset.y >= maxXAngle
+          ? maxXAngle
+          : offset.y <= -maxXAngle
+          ? -maxXAngle
+          : offset.y;
       last.y = pY;
       last.x = pX;
       controlCube.style = `--crx: ${-offset.y}deg; --cry: ${offset.x}deg;`;
@@ -30,34 +37,44 @@ function implementControlCubeMovement() {
     }
 
     function endCubeMovement() {
-      window.removeEventListener(isTouch ? 'touchmove' : 'mousemove', handleCubeMovement);
-      window.removeEventListener(isTouch ? 'touchend' : 'mouseup', endCubeMovement);
+      window.removeEventListener(
+        isTouch ? "touchmove" : "mousemove",
+        handleCubeMovement
+      );
+      window.removeEventListener(
+        isTouch ? "touchend" : "mouseup",
+        endCubeMovement
+      );
       lastOffset.x = offset.x;
       lastOffset.y = offset.y;
     }
 
-    window.addEventListener(isTouch ? 'touchmove' : 'mousemove', handleCubeMovement);
-    window.addEventListener(isTouch ? 'touchend' : 'mouseup', endCubeMovement);
+    window.addEventListener(
+      isTouch ? "touchmove" : "mousemove",
+      handleCubeMovement
+    );
+    window.addEventListener(isTouch ? "touchend" : "mouseup", endCubeMovement);
   }
 
-  controlCube.addEventListener('mousedown', allowCubeMovement);
-  controlCube.addEventListener('touchstart', allowCubeMovement);
+  controlCube.addEventListener("mousedown", allowCubeMovement);
+  controlCube.addEventListener("touchstart", allowCubeMovement);
 }
 
 function implementRubiksCubePiecesMovement() {
-  const piecesMatrix = new PiecesMatrix('.rubiks-cube');
+  const piecesMatrix = new PiecesMatrix(".rubiks-cube");
 
   function allowCubeMovement(startEvent) {
     const isTouch = startEvent.touches;
-    const { pageX: startX, pageY: startY } = isTouch ? startEvent.touches[0] : startEvent;
+    const { pageX: startX, pageY: startY } = isTouch
+      ? startEvent.touches[0]
+      : startEvent;
     const offset = { x: 0, y: 0 };
     const piece = startEvent.target;
-    let axis = '';
+    let axis = "";
     let rotated = false;
     const minOffsetDiff = 10;
 
     function handleCubeMovement(moveEvent) {
-      moveEvent.preventDefault();
       const { pageX, pageY } = isTouch ? moveEvent.touches[0] : moveEvent;
       offset.x = pageX - startX;
       offset.y = pageY - startY;
@@ -65,53 +82,69 @@ function implementRubiksCubePiecesMovement() {
       if (!axis) {
         const offsetDiff = Math.abs(Math.abs(offset.x) - Math.abs(offset.y));
         if (offsetDiff >= minOffsetDiff) {
-          axis = Math.abs(offset.x) > Math.abs(offset.y) ? 'y' : 'x';
+          axis = Math.abs(offset.x) > Math.abs(offset.y) ? "y" : "x";
         }
       }
 
       if (axis && !rotated) {
-        const angle = axis === 'x' ? (offset.y >= 0 ? -90 : 90) : offset.x >= 0 ? 90 : -90;
+        const angle =
+          axis === "x" ? (offset.y >= 0 ? -90 : 90) : offset.x >= 0 ? 90 : -90;
         piecesMatrix.rotatePiece({ piece, axis, angle });
         endCubeMovement();
       }
     }
 
     function endCubeMovement() {
-      window.removeEventListener(isTouch ? 'touchmove' : 'mousemove', handleCubeMovement);
-      window.removeEventListener(isTouch ? 'touchend' : 'mouseup', endCubeMovement);
+      window.removeEventListener(
+        isTouch ? "touchmove" : "mousemove",
+        handleCubeMovement
+      );
+      window.removeEventListener(
+        isTouch ? "touchend" : "mouseup",
+        endCubeMovement
+      );
     }
 
-    window.addEventListener(isTouch ? 'touchmove' : 'mousemove', handleCubeMovement);
-    window.addEventListener(isTouch ? 'touchend' : 'mouseup', endCubeMovement);
+    window.addEventListener(
+      isTouch ? "touchmove" : "mousemove",
+      handleCubeMovement
+    );
+    window.addEventListener(isTouch ? "touchend" : "mouseup", endCubeMovement);
   }
 
-  piecesMatrix.pieces.forEach((piece) => piece.addEventListener('mousedown', allowCubeMovement));
-  piecesMatrix.pieces.forEach((piece) => piece.addEventListener('touchstart', allowCubeMovement));
+  piecesMatrix.pieces.forEach((piece) =>
+    piece.addEventListener("mousedown", allowCubeMovement)
+  );
+  piecesMatrix.pieces.forEach((piece) =>
+    piece.addEventListener("touchstart", allowCubeMovement)
+  );
 }
 
 class PiecesMatrix {
   pieces = [];
-  _sides = ['front', 'right', 'left', 'back', 'top', 'bottom'];
+  _sides = ["front", "right", "left", "back", "top", "bottom"];
   _matrix = {};
   _initialRotationMap = {
-    front: '--rotation: ',
-    right: '--rotation: rotateY(90deg) ',
-    left: '--rotation: rotateY(270deg) ',
-    back: '--rotation: rotateY(180deg) ',
-    top: '--rotation: rotateX(90deg) ',
-    bottom: '--rotation: rotateX(270deg) ',
+    front: "--rotation: ",
+    right: "--rotation: rotateY(90deg) ",
+    left: "--rotation: rotateY(270deg) ",
+    back: "--rotation: rotateY(180deg) ",
+    top: "--rotation: rotateX(90deg) ",
+    bottom: "--rotation: rotateX(270deg) ",
   };
 
   constructor(rubiksCubeCSSSelector) {
     this._cube = document.querySelector(rubiksCubeCSSSelector);
-    if (!this._cube) throw new Error('Rubik Cube not found');
+    if (!this._cube) throw new Error("Rubik Cube not found");
 
     this._fillPiecesMatrix(rubiksCubeCSSSelector);
   }
 
   _fillPiecesMatrix(rubiksCubeCSSSelector) {
     this._sides.forEach((side) => {
-      const pieces = document.querySelectorAll(`${rubiksCubeCSSSelector} .${side} > *`);
+      const pieces = document.querySelectorAll(
+        `${rubiksCubeCSSSelector} .${side} > *`
+      );
       this._matrix[side] = [[], [], []];
       for (let i = 0; i < pieces.length; i += 1) {
         const piece = pieces[i];
@@ -121,7 +154,7 @@ class PiecesMatrix {
         piece._piecesMatrix = {
           lastRotation: this._initialRotationMap[side],
           baseSide: side,
-          baseAxis: { x: { axis: 'x' }, y: { axis: 'y' }, z: { axis: 'z' } },
+          baseAxis: { x: { axis: "x" }, y: { axis: "y" }, z: { axis: "z" } },
         };
       }
     });
@@ -129,15 +162,19 @@ class PiecesMatrix {
 
   rotatePiece({ piece, axis, angle }) {
     const location = this._findPieceLocation(piece);
-    if (location.side === 'top' || location.side === 'bottom') return;
+    if (location.side === "top" || location.side === "bottom") return;
 
-    const maps = rotationMap[location.side][axis][axis === 'x' ? location.column : location.row];
+    const maps =
+      rotationMap[location.side][axis][
+        axis === "x" ? location.column : location.row
+      ];
 
     maps.forEach((m) => {
       m.rows.forEach((row) => {
         m.columns.forEach((column) => {
           const piece = this._matrix[m.side][row][column].shift();
-          const direction = angle * (m.reverse ? -1 : 1) >= 0 ? 'increase' : 'decrease';
+          const direction =
+            angle * (m.reverse ? -1 : 1) >= 0 ? "increase" : "decrease";
           const move = matrixMovementMap[m.side][m.axis][direction];
           const nextSide = move.next || m.side;
           const nextAngle = m.reverse ? -parseInt(angle) : angle;
@@ -192,7 +229,11 @@ class PiecesMatrix {
       baseAxis.y = temp;
     }
 
-    baseAxis.x.reverse = move.reverseColumn ? !baseAxis.x.reverse : baseAxis.x.reverse;
-    baseAxis.y.reverse = move.reverseRow ? !baseAxis.y.reverse : baseAxis.y.reverse;
+    baseAxis.x.reverse = move.reverseColumn
+      ? !baseAxis.x.reverse
+      : baseAxis.x.reverse;
+    baseAxis.y.reverse = move.reverseRow
+      ? !baseAxis.y.reverse
+      : baseAxis.y.reverse;
   }
 }
